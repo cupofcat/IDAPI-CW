d92bdf9812fd77b37042e19bcff3e2bb6dd873b6
cupofcat <cupofcat@gmail.com>
2011-10-26 18:32:13 +0100

    Corrected prior + done CPT

--- IDAPICourseworkSkeleton.py
+++ IDAPICourseworkSkeleton.py
@@ -6,6 +6,7 @@ from numpy import *
 #
 # Coursework 1 begins here
 #
+
 # Function to compute the prior distribution of the variable root from the data set
 def Prior(theData, root, noStates):
     prior = zeros((noStates[root]), float )
@@ -13,18 +14,28 @@ def Prior(theData, root, noStates):
     for val in theData[0:, root]:
         prior[val] += 1
     print(prior)
-    prior /= theData.shape[root]
+    prior /= theData.shape[0]
     print(prior)
 # end of Coursework 1 task 1
     return prior
+
 # Function to compute a CPT with parent node varP and xchild node varC from the data array
 # it is assumed that the states are designated by consecutive integers starting with 0
 def CPT(theData, varC, varP, noStates):
     cPT = zeros((noStates[varC], noStates[varP]), float )
 # Coursework 1 task 2 should be inserte4d here
-   
+    vecC = theData[0:, varC]
+    vecP = theData[0:, varP]
+    for i in range(theData.shape[0]):
+        cPT[vecC[i], vecP[i]] += 1
 # end of coursework 1 task 2
+    print(cPT)
+    for col in cPT.transpose():
+        col /= sum(col)
+    print(cPT)
+        
     return cPT
+
 # Function to calculate the joint probability table of two variables in the data set
 def JPT(theData, varRow, varCol, noStates):
     jPT = zeros((noStates[varRow], noStates[varCol]), float )
@@ -32,6 +43,7 @@ def JPT(theData, varRow, varCol, noStates):
     
 # end of coursework 1 task 3
     return jPT
+
 #
 # Function to convert a joint probability table to a conditional probability table
 def JPT2CPT(aJPT):
@@ -49,9 +61,11 @@ def Query(theQuery, naiveBayes):
 
 # end of coursework 1 task 5
     return rootPdf
+
 #
 # End of Coursework 1
 #
+
 # Coursework 2 begins here
 #
 # Calculate the mutual information from the joint probability table of two variables
@@ -205,11 +219,12 @@ def PrincipalComponents(theData):
 #
 noVariables, noRoots, noStates, noDataPoints, datain = ReadFile("Neurones.txt")
 theData = array(datain)
-AppendString("results.txt","Coursework One Results by dfg")
-AppendString("results.txt","") #blank line
-AppendString("results.txt","The prior probability of node 0")
+#AppendString("results.txt","Coursework One Results by dfg")
+#AppendString("results.txt","") #blank line
+#AppendString("results.txt","The prior probability of node 0")
 prior = Prior(theData, 0, noStates)
-AppendList("results.txt", prior)
+cpt = CPT(theData, 1, 3, noStates)
+#AppendList("results.txt", prior)
 #
 # continue as described
 #
